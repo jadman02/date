ae7e44bd49254332dea4ab56c20c9fc13f84c05f
jadman02 <jadman02@hotmail.com>
2017-02-01 09:25:06 +1100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -24,39 +24,4 @@ function onDeviceReady() {
 alert('device ready');  
 }
 
- function loginFacebook(){
 
-var provider = new firebase.auth.FacebookAuthProvider();
-firebase.auth().signInWithPopup(provider).then(function(result) {
- // This gives you a Facebook Access Token. You can use it to access theFacebook API.
-   console.log(result);
-   var token = result.credential.accessToken;
-   f_token =  token;
-// The signed-in user info.
-  var user = result.user;
-  console.log(user);
-  
-
-  
-
-// Set the access token here
-
-
-  
-// ...
-}).catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  // The email of the user's account used.
-  var email = error.email;
-  // The firebase.auth.AuthCredential type that was used.
-  var credential = error.credential;
-  console.log(errorCode);
-  console.log(errorMessage);
-   console.log(email);
-
-  });
-    
-    
-}
