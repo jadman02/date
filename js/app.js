1eccda60f3326813fae1b8f9b0f68193eb74ac76
jadman02 <jadman02@hotmail.com>
2017-02-01 09:23:29 +1100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,11 +2,6 @@ $(document).ready(function () {
 
 document.addEventListener("deviceready", onDeviceReady, false);
 
-});
-
-
-function onDeviceReady() {
-
   // Initialize your app
 var myApp = new Framework7({init: false});
 
@@ -18,6 +13,13 @@ var view1 = myApp.addView('#view-1');
 
   
   myApp.init();
+  
+});
+
+
+function onDeviceReady() {
+
+  
   
 alert('device ready');  
 }
