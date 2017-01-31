0ce1b4c6bd41ac41fc5025a0917ff3f529ac6e4e
jadman02 <jadman02@hotmail.com>
2017-02-01 08:57:10 +1100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,20 +2,6 @@ document.addEventListener("deviceready", onDeviceReady, false);
 
 function onDeviceReady() {
 
-  // Initialize your app
-var myApp = new Framework7({init: false});
-
-// Export selectors engine
-var $$ = Dom7;
-
-// Add views
-var view1 = myApp.addView('#view-1');
-
-  
-  myApp.init();
-  
-
-
 //app
 
 //duck
