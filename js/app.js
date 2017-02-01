02f21ee5c10046b7839f9c735627bba0bdcb6a56
jadman02 <jadman02@hotmail.com>
2017-02-01 14:40:56 +1100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -19,8 +19,48 @@ var app = {
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
-        alert('yo');
-
-        console.log('Received Event: ' + id);
+      initappDateDuck();
     }
 };
+
+function initappDateDuck(){
+
+// Initialize your app
+var myApp = new Framework7({init: false});
+// Export selectors engine
+var $$ = Dom7;
+// Add views
+var view1 = myApp.addView('#view-1');
+myApp.init();
+    
+}
+
+function loginFacebook(){
+
+var provider = new firebase.auth.FacebookAuthProvider();
+firebase.auth().signInWithPopup(provider).then(function(result) {
+ // This gives you a Facebook Access Token. You can use it to access theFacebook API.
+   alert(result);
+   var token = result.credential.accessToken;
+   f_token =  token;
+// The signed-in user info.
+  var user = result.user;
+  alert(user);
+  
+
+}).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // The email of the user's account used.
+  var email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+  var credential = error.credential;
+  alert(errorCode);
+  alert(errorMessage);
+   alert(email);
+
+  });
+    
+    
+}
